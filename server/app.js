76fe752226830cf9cbe5f6182c15403df1355643
ManasTripathi07 <tripathiaman1854@gmail.com>
2025-06-16 13:27:14 +0530

    cors-commit
    Signed-off-by: ManasTripathi07 <tripathiaman1854@gmail.com>

--- server/app.js
+++ server/app.js
@@ -10,29 +10,19 @@ const app = express();
 
 // Middlewares
 // Built-In
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// // Third-Party
-// app.use(
-//   cors({
-//     // origin: [process.env.FRONTEND_URL],
-//     // origin: "https://learnity-1.vercel.app",
-//     origin: "*",
-//     credentials: true,
-//   })
-// );
-// app.use(morgan('dev'));
-// app.use(cookieParser());
-
 app.use(express.json());
-app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
+// Third-Party
 app.use(
   cors({
-    // origin:"http://localhost:3000",
-    origin:"*",
-    credentials:true,
-  }) 
-)
+    // origin: [process.env.FRONTEND_URL],
+    // origin: "https://learnity-1.vercel.app",
+    origin: "*",
+    credentials: true,
+  })
+);
+// app.use(morgan('dev'));
+app.use(cookieParser());
 
 
 
@@ -48,23 +38,9 @@ app.use('/api/v1/payments', paymentRoutes);
 app.use('/api/v1', miscRoutes);
 
 // Default catch all route - 404
-// app.all('*', (_req, res) => {
-//   res.status(404).send('Page Fetched Successfully');
-// });
-
-app.get("/", (req, res) => {
-	return res.json({
-		success:true,
-		message:'Your server is up and running....'
-	});
+app.all('*', (_req, res) => {
+  res.status(404).send('Page Fetched Successfully');
 });
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
-
 
 // Custom error handling middleware
 app.use(errorMiddleware);
