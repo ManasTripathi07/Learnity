93413986615f07ee6252e7dd01d466378b24b163
ManasTripathi07 <tripathiaman1854@gmail.com>
2025-06-17 20:28:36 +0530

    auth-updations
    Signed-off-by: ManasTripathi07 <tripathiaman1854@gmail.com>

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,12 +1,25 @@
+// import axios from "axios";
+
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+
+
+// const axiosInstance = axios.create();
+
+// axiosInstance.defaults.baseURL = BASE_URL;
+// axiosInstance.defaults.withCredentials = true;
+
+// export default axiosInstance;
+
+
+
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-
-
-const axiosInstance = axios.create();
-
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true, 
+});
 
 export default axiosInstance;
